1ba6c29c18f56ee3a31cfa4c3e6735f6c6bcb546
AnnaKrasilschikova <annaak07@mail.ru>
2017-10-14 19:02:49 +0300

    Initial version

---
+++ desk_calendar/js/script.js
@@ -0,0 +1,488 @@
+/////////////*******часы в виде объекта*******////////////
+
+function Clock(options) {
+
+  this.template = options.template;
+  var timer;
+}
+  Clock.prototype._render = function render() {
+  
+    var date = new Date();
+
+    var hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    var min = date.getMinutes();
+    if (min < 10) min = '0' + min;
+
+    var sec = date.getSeconds();
+    if (sec < 10) sec = '0' + sec;
+
+    var output = this.template.replace('h', hours).replace('m', min).replace('s', sec);
+
+ //   console.log(output);
+  }
+
+  Clock.prototype.stop = function() {
+    clearInterval(timer);
+  };
+
+  Clock.prototype.start = function() {
+    render();
+	timer = setInterval(render, 1000);
+  }
+
+  function ExtClock(options){
+//		console.log('ExtClockConstructor');
+		Clock.apply(this, arguments);
+		this._precision = options.precision;// || 1000;
+//		console.log('precision = ' + this._precision);
+	}
+	
+	ExtClock.prototype = Object.create(Clock.prototype);
+	
+	ExtClock.prototype.start = function(){
+//	console.log('ExtClock');
+		this._render();
+		var self = this;
+		this._timer = setInterval(function(){
+			self._render();
+			}, this._precision);
+	
+	}
+	
+  /*  var clock = new Clock({
+      template: 'h:m:s'
+    });
+	
+    clock.start(); */
+	var clock = new ExtClock({template: 'h:m:s',precision: 1000});
+//	clock.start();
+
+//////////////***************/////////////		
+	
+/*		function startGame(sz) {
+			for(var i=0; i < sz; i++){
+				var td = document.createElement("td");
+				var card = document.createElement("div");
+				card.
+			}
+		
+		}*/
+		var saved_img = "";
+		var saved_card = "";
+		var this_card = "";
+				
+		function onRotate(clicked_id) {
+			var idCurr = "#" + clicked_id;
+			console.log("click: " + idCurr);
+		//	saved_card = idCurr
+			 onCheck(clicked_id);
+	//		$(idCurr).toggleClass("flipped");
+	//		setTimeout(removeFlip, 800);
+		}
+		
+		function removeFlip(){ 
+			$(this_card).removeClass("flipped");
+			$(saved_card).removeClass("flipped");
+			saved_img = "";
+			saved_card = "";
+
+		}
+
+		function CheckOver() {
+			var isOver = true;
+			$(".card").each(function(index, element){
+				if(!$(element).hasClass("over")){
+					isOver = false;
+					return false;
+				}
+			})
+			if(isOver)
+				alert("Вы победили!");
+		}
+		
+		var arrFlipped = new Array();
+		function onCheck(clicked_id) {
+			var counter=0;
+			var strCurr = "#" + clicked_id;
+			
+			var curr_front = $(strCurr).children(".card_front");
+			//проверили счетчик
+			$(".card").each(function(index, element){
+					if($(element).hasClass("flipped") && !$(element).hasClass("over")){
+					counter++;
+					arrFlipped.push(element);
+				}
+			})
+			//если это первый клик, запомнили картинку
+			if(counter === 0) {
+				console.log(counter);
+				saved_img = $(strCurr).children(".card_front").first().css("background-image");
+				saved_card = strCurr;
+				$(saved_card).toggleClass("flipped");
+				console.log(saved_img);
+			}
+			//если это второй клик, сверили картинки и, если они совпали, отключили клики.
+			else if(counter === 1){
+			console.log(counter);
+				if($(curr_front).css("background-image") === saved_img && saved_card !== strCurr) {
+					console.log("===");
+					$(strCurr).toggleClass("flipped");
+					$(strCurr).unbind("click");
+					$(saved_card).unbind("click");
+					$(strCurr).addClass("over");
+					$(saved_card).addClass("over");
+				}
+				else if(saved_card === strCurr){
+					$(saved_card).toggleClass("flipped");
+				}else{
+					console.log("+++");
+					this_card = strCurr;
+					$(this_card).addClass("flipped");
+					console.log(strCurr);
+					
+					setTimeout(removeFlip, 800);
+				}
+			}
+			 setTimeout(CheckOver, 800);
+		}
+///////////////////////		
+		var year;
+		var day;
+		var wday;
+		var month;
+
+		function outputTime() {
+			var d = new Date();
+			var hour = d.getHours();
+			var min = d.getMinutes();
+			if(min < 10)
+				min = "0" + min;
+			
+			var sec = d.getSeconds();
+			if(sec < 10)
+				sec = "0" + sec;
+			document.getElementById("clock").innerHTML = hour + ":" + min + ":" + sec;
+		}
+		outputTime(); 
+		setInterval(outputTime, 1000);
+		
+		function outputDate(isToday) {
+			clearCalend();
+			var d = new Date();
+			if(isToday === "today"){
+				year = d.getFullYear();
+				month = d.getMonth();
+				day = d.getDate();
+				wday = d.getDay();
+			}
+			else
+				day = 0;
+			document.getElementById("year").innerHTML = year;
+			var arrMonth = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+			document.getElementById("month").innerHTML = arrMonth[month];
+			var wday_b = calcBeginMonth(1, month+1, year);
+			fillDays(wday_b);
+			changeBackForMonth();
+		}
+		outputDate("today");
+		
+		function calcBeginMonth(dy, mon, year) {
+			dy = parseInt(dy);
+			mon = parseInt(mon);
+			var a = parseInt((14-mon)/12, 10);
+			var y = year - a;
+			var m = mon + 12*a - 2;
+			var ret = (7000+parseInt(dy+y+parseInt(y/4, 10)-parseInt(y/100, 10)+parseInt(y/400, 10)+(31*m)/12, 10))%7;
+			return ret;
+		}
+		
+		function fillDays(wday_b) {
+			var d = 1;
+			var count;
+			switch(month)
+			{
+			case 0: case 2: case 4: case 6: case 7: case 9: case 11:
+				count = 31;
+				break;
+			case 1:
+				if(year%4==0)
+					count = 29;
+				else
+					count = 28;
+				break;
+			default:
+				count = 30;
+				break;
+			}
+			var id = "d";
+			var i = wday_b;
+			if(i === 0)
+				i = 7;
+			while(d <= count){
+				id = id + i;
+				document.getElementById(id).innerHTML = d;
+				if(Number(day) != 0 && day === d)
+					document.getElementById(id).style.color = "red";
+				d++;
+				i++;
+				id = "d";
+			}
+		}
+		
+		function clearCalend(){
+			var arr = document.getElementsByClassName("dat");
+			for(var i=0; i < arr.length; i++){
+				arr[i].innerHTML = "";
+				arr[i].style.color = "blue";
+			}
+		}
+		
+		function changeYear(num) {
+			year += num;
+			outputDate();
+		}
+		
+		function changeMonth(num) {
+			month += num;
+			if(month == 12)
+				month = 0;
+			if(month == -1)
+				month = 11;
+			outputDate();
+			changeBackForMonth();
+		}
+		
+		function changeBackForMonth() {
+			switch(month){
+				case 0:
+					document.body.style.backgroundImage = "url('img/winter2.jpg')";
+					break;
+				case 1:
+					document.body.style.backgroundImage = "url('img/winter3.jpg')";
+					break;
+				case 2:
+					document.body.style.backgroundImage = "url('img/spring1.jpg')";
+					break;
+				case 3:
+					document.body.style.backgroundImage = "url('img/spring2.jpg')";
+					break;
+				case 4:
+					document.body.style.backgroundImage = "url('img/spring3.jpg')";
+					break;
+				case 5:
+					document.body.style.backgroundImage = "url('img/summer1.jpg')";
+					break;
+				case 6:
+					document.body.style.backgroundImage = "url('img/summer2.jpg')";
+					break;
+				case 7:
+					document.body.style.backgroundImage = "url('img/summer3.jpg')";
+					break;
+				case 8:
+					document.body.style.backgroundImage = "url('img/autumn1.jpg')";
+					break;
+				case 9:
+					document.body.style.backgroundImage = "url('img/autumn2.jpg')";
+					break;
+				case 10:
+					document.body.style.backgroundImage = "url('img/autumn3.jpg')";				
+					break;
+				case 11:
+					document.body.style.backgroundImage = "url('img/winter1.jpg')";	
+					break;
+			}
+		}
+		
+		var arrCurrencies;// = new Array();
+        var uri = 'http://www.nbrb.by/API/';
+	    function ratestoday(p) {
+            $.getJSON(uri + 'ExRates/Rates', {'Periodicity': p })
+            .done(function (data) {
+				arrCurrencies = data;
+				for(var i=0; i < data.length; i++){
+					if(data[i].Cur_Abbreviation === "EUR" || data[i].Cur_Abbreviation === "USD" || data[i].Cur_Abbreviation === "RUB" || data[i].Cur_Abbreviation === "JPY") {
+						var row = document.createElement("tr");
+
+						var col1 = document.createElement("td");
+			//			col1.innerHTML = data[i].Cur_Abbreviation;
+
+						var img = document.createElement("img");
+						
+						var col2 = document.createElement("td");
+						col2.innerHTML = data[i].Cur_Scale + " " + data[i].Cur_Name;
+
+						var col3 = document.createElement("td");
+						col3.id = data[i].Cur_Abbreviation;
+						col3.innerHTML = data[i].Cur_OfficialRate;	
+						
+						$(".res").append(row);
+						var cls = data[i].Cur_Abbreviation;
+						$(".res tr:last-of-type").append(col1);
+						$(".res tr:last-of-type td:last-child").append(img);					
+						$(".res td:last-child img").addClass("flag");
+						$(".res td:last-child img").addClass(cls);
+						$(".res tr:last-of-type").append(col2);
+						$(".res tr:last-of-type").append(col3);
+					}
+					else
+						continue;
+				}
+				
+		    }).error(function (err) {alert('ошибка');});
+        };
+		ratestoday(0);
+		
+		function myMap() {
+			var mapOptions = {
+				center: new google.maps.LatLng(53.90, 27.41),
+				zoom: 17,
+				mapTypeId: google.maps.MapTypeId.HYBRID
+			}
+			var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+		}
+///////weather//////////////	
+//		localStorage.setItem("place", "Minsk");
+		var placeArray = new Array();
+		getStorage();
+		function getWeather(plc) {
+			if(plc.length == 0)
+			{
+				plc = "Minsk";
+				fillStorage("Minsk");
+			}
+			var str = "http://api.openweathermap.org/data/2.5/weather?q=" + plc + "&units=metric&APPID=d6d80fd647a85398205dd2295e192f7c";
+//			console.log(str);
+			$.getJSON(str)
+//			$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Minsk&units=metric&APPID=fe464d365c95c2f80dbcc6cbe99c84e7')
+			.done(function (data) {
+				document.getElementById("grad").innerHTML = "&nbsp" + "&nbsp" + "&nbsp" + data.main.temp + '&deg' + "C";
+				document.getElementById("humid").innerHTML = "&nbsp" + "&nbsp" + "&nbsp" + data.main.humidity + "%";
+				document.getElementById("pres").innerHTML = "&nbsp" + "&nbsp" + "&nbsp" + data.main.pressure + "гПа";
+				//Clear = ясно
+				if(data.weather[0].main === "Clear")
+					document.getElementById("pic").style.backgroundImage = "url(img/sun.png)";
+				else if(data.weather[0].main === "Rain")
+					document.getElementById("pic").style.backgroundImage = "url(img/rain.png)";
+				else if(data.weather[0].main === "Snow")
+					document.getElementById("pic").style.backgroundImage = "url(img/snow.png)";
+				else if(data.weather[0].main === "Clouds")
+					document.getElementById("pic").style.backgroundImage = "url(img/cloud.png)";
+				else if(data.weather[0].main === "Mist" || data.weather[0].main === "Haze")
+					document.getElementById("pic").style.backgroundImage = "url(img/mist.png)";				
+//				console.log(data.weather[0].main);
+			}).error(function (err) {alert('ошибка');});
+		}
+		getWeather(placeArray[0]);
+		
+		function fillStorage(str) {
+			var strStorage = placeArray.join(",");
+			strStorage += ",";
+			strStorage += str;
+			localStorage.place = strStorage;
+		}
+
+		function getStorage() {
+			var strStorage = new String();
+			strStorage = localStorage.getItem("place");
+			if(strStorage === null)
+				return;
+			if(strStorage.length != 0)
+				placeArray = strStorage.split(",");
+//			console.log(placeArray);
+		}
+		
+		function addPlace() {
+			var val = document.getElementById("newPlace").value;
+	//		if(val.length > 0){
+	//			placeArray[placeArray.length] = val;
+	//		}
+			fillStorage(val);
+			document.getElementById("newPlace").value = "";
+			fillPlacesList();
+		}
+		
+		function clearPlaceSelect() {
+			var listPlaces = document.getElementById("place").getElementsByTagName("option");
+		
+			for(var i=listPlaces.length; i > 0 ; i--)
+				document.getElementById("place").removeChild(listPlaces[i-1]);
+		}
+		
+		function fillPlacesList() {
+			clearPlaceSelect();
+			getStorage();
+			for(var i=0; i < placeArray.length; i++){
+				var tmp = document.createElement("option");
+				tmp.innerHTML = placeArray[i];
+				document.getElementById("place").appendChild(tmp);
+			}
+		}
+		fillPlacesList();
+		
+		function choosePlace(){
+			plc = document.getElementById("place").value;
+			getWeather(plc);
+		}
+////////////////////////////////////////////////////////////////////////////////	
+		function output(){
+			var i=0;
+			var str;
+			for(i=0; i < 100; i++)
+			{
+				if( (i%3) == 0 && (i%5) !=0)
+					str+="Fuzz,";
+				else if( (i%3) != 0 && (i%5) == 0)
+					str+="Buzz,";
+				else if((i%3) == 0 && (i%5) == 0)
+					str += "FuzzBuzz,";
+				else
+					str += i + ",";
+			}
+			console.log(str);
+		}
+	
+		function funcChange() {
+			var idCurr = document.getElementById("listCurr").value;
+			for(var i=0; i < arrCurrencies.length; i++)
+			{
+				if(arrCurrencies[i].Cur_Abbreviation !== idCurr)
+					continue;
+				
+				document.getElementById("valCurr").innerHTML = arrCurrencies[i].Cur_OfficialRate;
+				break;
+			}
+			if(document.getElementById("quantity").value !== "")
+				funcCalc();
+		}
+		
+		function funcCalc() {
+			var quan = document.getElementById("quantity").value;
+			var res = 0;
+			var currAbbr = document.getElementById("listCurr").value;
+			console.log(currAbbr);
+			for(var i=0; i < arrCurrencies.length; i++)
+			{
+				if(arrCurrencies[i].Cur_Abbreviation !== currAbbr)
+					continue;
+				res = arrCurrencies[i].Cur_OfficialRate * quan / arrCurrencies[i].Cur_Scale;
+				break;
+			}
+			document.getElementById("result").innerHTML = res.toFixed(2);
+		}
+/*		
+		function makeCounter(){
+			var currentCount = 1;
+		//	console.log("Scope: " + currentCount);
+			return function(){
+				currentCount++;
+			//	console.log("inside: " + currentCount);
+				return currentCount;
+			}
+		}
+		
+		var counter = makeCounter();
+		
+		console.log(counter());
+*/		
